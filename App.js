c42bca371b56b629e8c1c91604c2628ea144653b
Jason Crawford <daxapps777@gmail.com>
2019-05-31 12:02:38 -0400

    Add Pages

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from "./screens/HomeScreen";
+import AddNewContactScreen from "./screens/AddNewContactScreen";
+import EditContactScreen from "./screens/EditContactScreen";
+import ViewContactScreen from "./screens/ViewContactScreen";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+const MainNavigator = createStackNavigator(
+  {
+    Home: {screen: HomeScreen},
+    Add: {screen: AddNewContactScreen},
+    View: {screen: ViewContactScreen},
+    Edit: {screen: EditContactScreen}
+  }, 
+  {
+    defaultNavigationOptions: {
+      headerTintColor: "#fff",
+      headerStyle: {
+        backgroundColor: "#b83227"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
   }
-}
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = createAppContainer(MainNavigator);
+export default App;
