2fd1788c6ad58eeeae416ee51785e199299c4fbd
Jason Crawford <daxapps777@gmail.com>
2019-06-03 11:11:00 -0400

    enable delete contact

--- screens/ViewContactScreen.js
+++ screens/ViewContactScreen.js
@@ -6,7 +6,7 @@ import { Entypo } from '@expo/vector-icons';
 export default class ViewContactScreen extends React.Component {
   constructor(props){
     super(props);
-    this.sate = {
+    this.state = {
       fname: "DummyText",
       lname: "DummyText",
       phone: "DummyText",
@@ -32,7 +32,7 @@ export default class ViewContactScreen extends React.Component {
     await AsyncStorage.getItem(key)
       .then(contactjsonString => {
         var contact = JSON.parse(contactjsonString);
-        contact[key] = key;
+        contact["key"] = key;
         this.setState(contact)
       })
       .catch(error => {
@@ -77,11 +77,93 @@ export default class ViewContactScreen extends React.Component {
       })
   }
 
+  editContact = (key) => {
+    this.props.navigation.navigate("Edit", { key: key})
+  };
+
+  deleteContact = key => {
+    Alert.alert(
+      "Delete Contact?",
+      `${this.state.fname} ${this.state.lname}`,
+      [
+        { text: "Cancel", onPress: () => console.log("cancel pressed")},
+        {
+          text: "OK",
+          onPress: async () => {
+            await AsyncStorage.removeItem(key)
+            .then( () => {
+              this.props.navigation.goBack();
+            })
+            .catch(error => {
+              console.log(error);
+            });
+          }
+        }
+      ]
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>View Contact</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.contactIconContainer}>
+          <Text style={styles.contactIcon}>{this.state.fname[0].toUpperCase()}</Text> 
+          <View style={styles.nameContainer}>
+            <Text style={styles.name}>{this.state.fname} {this.state.lname}</Text>
+          </View>
+        </View>
+        <View style={styles.infoContainer}>
+          <Card>
+            <CardItem bordered>
+              <Text style={styles.infoText}>Phone</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Text style={styles.infoText}>{this.state.phone}</Text>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem bordered>
+              <Text style={styles.infoText}>Email</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Text style={styles.infoText}>{this.state.email}</Text>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem bordered>
+              <Text style={styles.infoText}>Address</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Text style={styles.infoText}>{this.state.address}</Text>
+            </CardItem>
+          </Card>
+        </View>
+        <Card style={styles.actionContainer}>
+          <CardItem style={styles.actionButton}>
+            <TouchableOpacity onPress={() => {this.smsAction(this.state.phone)}}>
+              <Entypo name="message" size={50} color="#BB3227" />
+            </TouchableOpacity>
+          </CardItem>
+          <CardItem style={styles.actionButton}>
+            <TouchableOpacity onPress={() => {this.callAction(this.state.phone)}}>
+              <Entypo name="phone" size={50} color="#BB3227" />
+            </TouchableOpacity>
+          </CardItem>
+        </Card>
+        <Card style={styles.actionContainer}>
+          <CardItem style={styles.actionButton}>
+            <TouchableOpacity onPress={() => {this.editContact(this.state.key)}}>
+              <Entypo name="edit" size={50} color="#BB3227" />
+            </TouchableOpacity>
+          </CardItem>
+          <CardItem style={styles.actionButton}>
+            <TouchableOpacity onPress={() => {this.deleteContact(this.state.key)}}>
+              <Entypo name="trash" size={50} color="#BB3227" />
+            </TouchableOpacity>
+          </CardItem>
+        </Card>
+        
+      </ScrollView>
     );
   }
 }
@@ -89,8 +171,50 @@ export default class ViewContactScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  contactIconContainer: {
+    height: 200,
+    backgroundColor: "#B83227",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  contactIcon: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  nameContainer: {
+    width: "100%",
+    height: 70,
+    padding: 10,
+    backgroundColor: "rgba(255,255,255,0.5)",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 0
+  },
+  name: {
+    fontSize: 24,
+    color: "#000",
+    fontWeight: "900"
+  },
+  infoText: {
+    fontSize: 18,
+    fontWeight: "300"
+  },
+  actionContainer: {
+    flexDirection: "row"
+  },
+  actionButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  actionText: {
+    color: "#B83227",
+    fontWeight: "900"
+  },
+  infoContainer: {
+    flexDirection: "column"
+  }
 });
